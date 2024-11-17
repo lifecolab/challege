034e7389eae20f34b0df368f502ffd2f52b39a56
DESKTOP-4DSVN3C\sda92 <sda923@naver.com>
2024-11-17 19:13:49 +0900

    20241117 초기화

---
+++ src/main/java/com/lifecolab/challengeMaker/ChallengeMakerApplication.java
@@ -0,0 +1,13 @@
+package com.lifecolab.challengeMaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeMakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lifecolab/challengeMaker/ChallengeMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifecolab.challengeMaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
